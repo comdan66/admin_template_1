2ea640afaaeae98f0015f6e9f8485eab7163e89e
OA Wu <comdan66@gmail.com>
2017-05-08 18:10:32 +0800

    Fix code.

--- js/public.js
+++ js/public.js
@@ -15,4 +15,14 @@ $(function () {
 
   $('.table-list').each (function () { if (!$(this).find ('tbody > tr').length) $(this).find ('tbody').append ($('<tr />').append ($('<td />').attr ('colspan', $(this).find ('thead th').length).text ('沒有任何資料。'))); });
 
+  $('textarea.cke').ckeditor ({
+    filebrowserUploadUrl: '',
+    filebrowserImageBrowseUrl: '',
+    skin: 'oa',
+    height: 300,
+    resize_enabled: false,
+    removePlugins: 'elementspath',
+    toolbarGroups: [{ name: '1', groups: [ 'mode', 'tools', 'links', 'basicstyles', 'colors', 'insert', 'list' ] }],
+    removeButtons: 'Strike,Underline,Italic,Table,HorizontalRule,Smiley,Subscript,Superscript,Forms,Save,NewPage,Print,Preview,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,SelectAll,Scayt,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Form,RemoveFormat,CreateDiv,BidiLtr,BidiRtl,Language,Anchor,Flash,PageBreak,Iframe,About,Styles'
+  });
 });
